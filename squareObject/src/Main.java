91dc424525120ba491123f6a41a7219326cfadaa
samimays <sami.mays93@gmail.com>
2021-03-25 18:19:36 -0500

    newest changes to the square file

---
+++ squareObject/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello there! Would you like to create a " +
+                "square or a rectangle? Please answer square or rectangle");
+        String shape = input.next();
+
+    }
+}
